a1856c848bbc35fb03e75a5990f8b5ddf2ffb569
gsmithw <54960686+gsmithw@users.noreply.github.com>
2019-11-26 13:15:21 -0500

    Add files via upload

--- js/challenge3.js
+++ js/challenge3.js
@@ -22,13 +22,35 @@
 
   $("form").submit(function( event ) {
 
-    var fruit = $("input[name='fruit']")
-
-    for (let i = 0; i < fruit.length;i++){
-        if (fruit[i].checked)
-            return true;
+    if ($("input[name='fruit']").is(':checked')){
+      return true;
     }
-    alert("You must pick a fruit!")
+
+    else {
+      alert("You must pick a fruit!")
+    }
+
+
+    if ($("input[name='standing']").is(':checked')){
+      return true;
+    }
+
+    else {
+      alert("You must pick a standing!")
+    }
+
     return false;
 
   })
+  //
+  //   var fruit = $("input[name='fruit']")
+  //   var stand = $("input[name='standing']")
+  //
+  //   for (let i = 0; i < fruit.length;i++){
+  //       if (fruit[i].checked)
+  //           return true;
+  //   }
+  //   alert("You must pick a fruit!")
+  //   return false;
+  //
+  // })
